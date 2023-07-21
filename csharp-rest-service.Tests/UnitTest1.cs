6e54a31772f9ccde0b077f1ba8f8e53b49a896cf
Olivier Mocquais <omocquais@vmware.com>
2023-07-21 13:54:49 +0200

    initial commit

---
+++ csharp-rest-service.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace csharp_rest_service.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    { 
+        bool result = true;
+        Assert.False(result, "1 should not be true");
+
+    }
+}
