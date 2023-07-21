d78ea1c1c57e39696c1de2421f2a9bff7a37c276
Olivier Mocquais <omocquais@vmware.com>
2023-07-21 14:36:28 +0200

    the test should pass

--- csharp-rest-service.Tests/UnitTest1.cs
+++ csharp-rest-service.Tests/UnitTest1.cs
@@ -7,7 +7,7 @@ public class UnitTest1
     [Fact]
     public void Test1()
     { 
-        bool result = true;
+        bool result = false;
         Assert.False(result, "1 should not be true");
 
     }
